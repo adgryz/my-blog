5d601853f8880575d18f73fd6caebc594c1bce91
Adam Gryz <agryz@pretius.com>
2020-02-17 15:47:25 +0100

    auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,5 @@
 import React from "react"
-import netlify from "netlify-auth-providers"
 
 export default () => {
-  const handleLogin = e => {
-    e.preventDefault()
-    const authenticator = new netlify.default({})
-
-    authenticator.authenticate(
-      { provider: "github", scope: "user" },
-      (err, data) => {
-        err
-          ? console.log("Error Authenticating with GitHub: " + err)
-          : console.log(
-              "Authenticated with GitHub. Access Token: " + data.token
-            )
-      }
-    )
-  }
-
-  return (
-    <div>
-      <div>Hello world!</div>
-      <div onClick={handleLogin}>Login</div>
-    </div>
-  )
+  return <div>Hello world!</div>
 }
